641da44c9610fb01343196199045a3b04458c29c
Swapnil* <sw4pn@yahoo.com>
2023-06-07 12:43:25 +0530

    loader ui correction.

--- src/components/loaders/LoaderUI.tsx
+++ src/components/loaders/LoaderUI.tsx
@@ -1,6 +1,6 @@
 const LoaderUI = () => {
   return (
-    <div className="fixed top-1/2 left-1/2">
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div className="relative inline-block w-12 h-12">
         <span className="absolute content-[''] w-16 h-16 rounded-full animate-puff bg-black"></span>
         <span
