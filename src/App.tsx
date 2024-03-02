25f9f1f15d27a3ad59b665cd98ee94ef8af45c69
Swapnil Andhale <sw4pn@yahoo.com>
2024-03-02 18:46:50 +0530

    fix: loading issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,51 @@
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import RouteHandler from "./routes/RouteHandler";
 // import ServerError from "./components/ServerError";
 import LoaderUI from "./components/loaders/LoaderUI";
-import { useDispatch } from "react-redux";
-import { loadUser } from "./features/auth/authSlice";
-import { Dispatch } from "@reduxjs/toolkit";
+// import { useDispatch } from "react-redux";
+// import { loadUser } from "./features/auth/authSlice";
+// import { Dispatch } from "@reduxjs/toolkit";
 // import { connectError } from "./utils/utils";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(true);
-  const dispatch: Dispatch<any> = useDispatch();
+  // const [isLoaded, setIsLoaded] = useState(true);
+  // const dispatch: Dispatch<any> = useDispatch();
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        await dispatch(loadUser());
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     try {
+  //       await dispatch(loadUser());
 
-        // action
-        //   ? connectError(action?.error?.code)
-        //     ? setServerActive(false)
-        //     : setServerActive(true)
-        //   : setServerActive(false);
-      } catch (error) {
-        // setServerActive(false);
-      } finally {
-        setIsLoaded(false);
-      }
-    };
+  //       // action
+  //       //   ? connectError(action?.error?.code)
+  //       //     ? setServerActive(false)
+  //       //     : setServerActive(true)
+  //       //   : setServerActive(false);
+  //     } catch (error) {
+  //       // setServerActive(false);
+  //     } finally {
+  //       setIsLoaded(false);
+  //     }
+  //   };
 
-    fetchUser();
-  }, [dispatch]);
+  //   fetchUser();
+  // }, [dispatch]);
 
   return (
     <>
-      {!isLoaded ? (
+      {/* {!isLoaded ? (
         <>
           <RouteHandler />
           <Toaster position="top-right" />
         </>
       ) : (
         <LoaderUI />
-      )}
+      )} */}
+      <Suspense fallback={<LoaderUI />}>
+        <RouteHandler />
+      </Suspense>
+      <Toaster position="top-right" />
     </>
   );
 }
